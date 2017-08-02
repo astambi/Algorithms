7639823af4a14b32a399d0717fbb7153a4000741
ahamidou <ahamidou@outlook.com>
2017-08-02 16:55:15 -0400

    Update RecursiveArraySum.cs

--- 01-Recursion/L01-RecursiveArraySum/RecursiveArraySum.cs
+++ 01-Recursion/L01-RecursiveArraySum/RecursiveArraySum.cs
@@ -1,27 +1,49 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace L01_RecursiveArraySum
 {
+    public class RecursiveArrayCalculator
+    {
+        private readonly string _numbersSpaceDelimitedText;
+
+        public RecursiveArrayCalculator(string numbersSpaceDelimitedText)
+        {
+            _numbersSpaceDelimitedText = numbersSpaceDelimitedText;
+        }
+
+        public long Calculate()
+        {
+            var array = GetNumbersArray();
+            return Sum(array);
+        }
+
+        private static long Sum(int[] numbers)
+        {
+            long sum = 0;
+            for (var i = numbers.Length; i > 0; i--)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        private int[] GetNumbersArray()
+        {
+            return _numbersSpaceDelimitedText.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+
     public class RecursiveArraySum
     {
         public static void Main()
         {
-            var numbers = Console.ReadLine()
-                         .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                         .Select(int.Parse)
-                         .ToArray();
-            var sum = Sum(numbers, 0);
+            var numbersSpaceDelimitedText = Console.ReadLine();
+            var calculator = new RecursiveArrayCalculator(numbersSpaceDelimitedText);
+            var sum = calculator.Calculate();
             Console.WriteLine(sum);
-        }
-
-        private static int Sum(int[] numbers, int index)
-        {
-            if (index == numbers.Length - 1)
-            {
-                return numbers[index];
-            }
-            return numbers[index] + Sum(numbers, index + 1);
         }
     }
 }
