3085cb69940169e5e9866974c1117fe2f23a0923
Anna <annsta@gmail.com>
2018-03-25 18:38:24 +0300

    Greedy Algorithms

---
+++ 04-Greedy Algorithms Lab/Activities/Activities.cs
@@ -0,0 +1,62 @@
+﻿namespace Activities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Activities
+    {
+        private class Activity
+        {
+            public int StartTime { get; set; }
+
+            public int EndTime { get; set; }
+        }
+
+        public static void Main()
+        {
+            var activities = InitializeActivities();
+            SelectActivities(activities);
+        }
+
+        private static void SelectActivities(List<Activity> activities)
+        {
+            if (activities.Any())
+            {
+                var earliestEndingActivity = activities.First();
+                Print(earliestEndingActivity);
+
+                for (int i = 1; i < activities.Count; i++)
+                {
+                    var currentActivity = activities[i];
+                    if (currentActivity.StartTime >= earliestEndingActivity.EndTime)
+                    {
+                        earliestEndingActivity = currentActivity;
+                        Print(earliestEndingActivity);
+                    }
+                }
+            }
+        }
+
+        private static void Print(Activity activity)
+            => Console.WriteLine($"Activity: {activity.StartTime} - {activity.EndTime}");
+
+        private static List<Activity> InitializeActivities()
+        {
+            var startingTimes = new[] { 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12 };
+            var endingTimes = new[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            var activities = new List<Activity>();
+
+            for (int i = 0; i < startingTimes.Length; i++)
+            {
+                activities.Add(new Activity
+                {
+                    StartTime = startingTimes[i],
+                    EndTime = endingTimes[i]
+                });
+            }
+
+            return activities.OrderBy(a => a.EndTime).ToList();
+        }
+    }
+}
---
+++ 04-Greedy Algorithms Lab/SumOfCoins/SumOfCoins.cs
@@ -0,0 +1,54 @@
+﻿namespace SumOfCoins
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumOfCoins
+    {
+        public static void Main(string[] args)
+        {
+            var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
+            var targetSum = 923;
+
+            var selectedCoins = ChooseCoins(availableCoins, targetSum);
+
+            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+            foreach (var selectedCoin in selectedCoins)
+            {
+                Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+            }
+        }
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+            coins = coins.OrderByDescending(x => x).ToList();
+
+            var selectedCoins = new Dictionary<int, int>();
+            var currentSum = 0;
+            var coinIndex = 0;
+
+            while (currentSum != targetSum && coinIndex < coins.Count)
+            {
+                var coinValue = coins[coinIndex];
+                var remaininigSum = targetSum - currentSum;
+                var coinsToTake = remaininigSum / coinValue;
+
+                if (coinsToTake != 0)
+                {
+                    selectedCoins[coinValue] = coinsToTake;
+                    currentSum += coinValue * coinsToTake;
+                }
+
+                coinIndex++;
+            }
+
+            if (currentSum != targetSum)
+            {
+                throw new InvalidOperationException("Greedy algorithm cannot produce desired sum with specified coins.");
+            }
+
+            return selectedCoins;
+        }
+    }
+}
