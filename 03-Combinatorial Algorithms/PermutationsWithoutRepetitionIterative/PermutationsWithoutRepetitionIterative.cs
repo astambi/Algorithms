6c494acd22d1bbf378d8d0c3d047c6760781317b
Anna <annsta@gmail.com>
2018-03-27 02:49:11 +0300

    Combinatorial Algorithms

---
+++ 03-Combinatorial Algorithms/PermutationsWithoutRepetitionIterative/PermutationsWithoutRepetitionIterative.cs
@@ -0,0 +1,89 @@
+﻿namespace PermutationsWithoutRepetitionIterative
+{
+    using System;
+
+    public class PermutationsWithoutRepetitionIterative
+    {
+        private static string[] elements;
+
+        public static void Main()
+        {
+            ReadElements();
+
+            CountdownQuickPermAlgorithm();
+            //CountingQuickPermAlgorithm();
+        }
+
+        private static void CountingQuickPermAlgorithm()
+        {
+            Print();
+
+            // Initialize controller array
+            var p = new int[elements.Length]; // n => [0,0,...,0]
+
+            var upper = 1; // initialize upper index = 1
+
+            while (upper < elements.Length) // n
+            {
+                if (p[upper] < upper)
+                {
+                    var lower = IsOdd(upper) ? p[upper] : 0;
+                    Swap(lower, upper);
+                    Print();
+
+                    p[upper]++;
+                    upper = 1; // reset upper index
+                }
+                else
+                {
+                    p[upper++] = 0; // reset to zero value
+                }
+            }
+        }
+
+        private static void CountdownQuickPermAlgorithm()
+        {
+            Print();
+            
+            // Initialize controller array
+            var p = new int[elements.Length + 1]; // [0,1,...,n-1]
+            for (int i = 0; i < p.Length; i++)
+            {
+                p[i] = i;
+            }
+
+            var upper = 1; // initialize upper index = 1
+
+            while (upper < elements.Length) // n
+            {
+                p[upper]--;
+
+                var lower = IsOdd(upper) ? p[upper] : 0;
+                Swap(lower, upper);
+                Print();
+
+                upper = 1; // reset upper index
+                while (p[upper] == 0)
+                {
+                    p[upper] = upper++; // reset zero values in controller array
+                }
+            }
+        }
+
+        private static bool IsOdd(int index)
+            => index % 2 != 0;
+
+        private static void Print()
+             => Console.WriteLine(string.Join(" ", elements));
+
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+
+        private static void ReadElements()
+            => elements = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
---
+++ 03-Combinatorial Algorithms/Snakes/Snakes.cs
@@ -0,0 +1,139 @@
+﻿namespace Snakes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Snakes
+    {
+        private const char Up = 'U';
+        private const char Down = 'D';
+        private const char Left = 'L';
+        private const char Right = 'R';
+
+        private static int snakesCount;
+        private static char[] snakePath;
+        private static HashSet<string> snakes = new HashSet<string>();
+        private static HashSet<string> visitedPositions = new HashSet<string>();
+
+        public static void Main()
+        {
+            InitializeSnakePath();
+
+            GenerateSnake(1, 0, 1, Right); // Priority R D L U
+
+            Console.WriteLine($"Snakes count = {snakesCount}");
+        }
+
+        private static void GenerateSnake(int index, int row, int col, char direction)
+        {
+            if (index >= snakePath.Length)
+            {
+                var snake = new string(snakePath);
+
+                if (snakePath.Length == 1)
+                {
+                    Console.WriteLine(snake);
+                    return;
+                }
+
+                if (!snakes.Contains(snake))
+                {
+                    MarkIdentical(snake);
+                }
+            }
+            else
+            {
+                var currentPosition = $"{row} {col}";
+
+                if (!visitedPositions.Contains(currentPosition))
+                {
+                    visitedPositions.Add(currentPosition);
+                    snakePath[index] = direction;
+
+                    // Observe Priority: R D L U
+                    GenerateSnake(index + 1, row, col + 1, Right);
+                    GenerateSnake(index + 1, row + 1, col, Down);
+                    GenerateSnake(index + 1, row, col - 1, Left);
+                    GenerateSnake(index + 1, row - 1, col, Up);
+
+                    visitedPositions.Remove(currentPosition);
+                }
+            }
+        }
+
+        private static void MarkIdentical(string snake)
+        {
+            var reversed = Reverse(snake);
+            var flipped = Flip(snake);
+
+            if (!snakes.Contains(reversed) && !snakes.Contains(flipped))
+            {
+                snakes.Add(reversed);
+                snakes.Add(flipped);
+
+                Console.WriteLine(snake);
+                snakesCount++;
+            }
+        }
+
+        private static char[] Rotate(char[] snake)
+        {
+            while (snake[1] != Right)
+            {
+                for (int i = 0; i < snake.Length; i++)
+                {
+                    switch (snake[i])
+                    {
+                        case Right: snake[i] = Down; break;
+                        case Down: snake[i] = Left; break;
+                        case Left: snake[i] = Up; break;
+                        case Up: snake[i] = Right; break;
+                        default: break;
+                    }
+                }
+            }
+
+            return snake;
+        }
+
+        private static string Flip(string snake)
+        {
+            var flipped = new char[snake.Length];
+
+            for (int i = 0; i < snake.Length; i++)
+            {
+                switch (snake[i])
+                {
+                    case Up: flipped[i] = Down; break;
+                    case Down: flipped[i] = Up; break;
+                    default: flipped[i] = snake[i]; break;
+                }
+            }
+
+            return new string(Rotate(flipped)); // return right rotated
+        }
+
+        private static string Reverse(string snake)
+        {
+            var reversed = new char[snake.Length];
+            reversed[0] = snake[0];
+
+            for (int i = 1; i < snake.Length; i++)
+            {
+                reversed[i] = snake[snake.Length - i];
+            }
+
+            return new string(Rotate(reversed)); // return right rotated
+        }
+
+        private static void InitializeSnakePath()
+        {
+            var length = int.Parse(Console.ReadLine());
+
+            snakePath = new char[length];
+            snakePath[0] = 'S';
+
+            visitedPositions.Add($"0 0");
+        }
+    }
+}
