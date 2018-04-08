f281fc8d844a2cb78ff1ad906f53d6291c2d97e6
Anna <annsta@gmail.com>
2018-04-09 01:07:16 +0300

    Dynamic Programming

---
+++ 05-Dynamic Programming/Fibonacci/Fibonacci.cs
@@ -0,0 +1,52 @@
+﻿namespace Fibonacci
+{
+    using System;
+
+    public class Fibonacci
+    {
+        private static long[] fibonacci;
+
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            fibonacci = new long[n + 1];
+
+            //Console.WriteLine(FibonaciIteration(n));
+            Console.WriteLine(FibonaciRecursion(n));
+        }
+
+        private static long FibonaciIteration(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1 || i == 2)
+                {
+                    fibonacci[i] = 1;
+                }
+                else
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+            }
+
+            return fibonacci[n];
+        }
+
+        private static long FibonaciRecursion(int n)
+        {
+            if (fibonacci[n] != 0)
+            {
+                return fibonacci[n];
+            }
+
+            if (n == 1 || n == 2)
+            {
+                fibonacci[n] = 1;
+                return 1;
+            }
+
+            fibonacci[n] = FibonaciRecursion(n - 1) + FibonaciRecursion(n - 2);
+            return fibonacci[n];
+        }
+    }
+}
---
+++ 05-Dynamic Programming/LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,65 @@
+﻿namespace LongestIncreasingSubsequence
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LongestIncreasingSubsequence
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var solutionsLen = new int[numbers.Length];
+            var prevIndices = new int[numbers.Length];
+
+            var maxSolutionLen = 0;
+            var maxSolutionLenIndex = 0;
+
+            for (int current = 0; current < numbers.Length; current++)
+            {
+                var solutionLen = 1;
+                var prevIndex = -1;
+                var currentNumber = numbers[current];
+
+                for (int solIndex = 0; solIndex < current; solIndex++)
+                {
+                    var prevNumber = numbers[solIndex];
+                    var prevSolutionLen = solutionsLen[solIndex];
+
+                    if (currentNumber > prevNumber
+                        && solutionLen <= prevSolutionLen)
+                    {
+                        solutionLen = prevSolutionLen + 1;
+                        prevIndex = solIndex;
+                    }
+                }
+
+                solutionsLen[current] = solutionLen;
+                prevIndices[current] = prevIndex;
+
+                if (solutionLen > maxSolutionLen)
+                {
+                    maxSolutionLen = solutionLen;
+                    maxSolutionLenIndex = current;
+                }
+            }
+
+            // Reconstruct Longest Increasing Seq
+            var longestSeq = new Stack<int>();
+            var index = maxSolutionLenIndex;
+
+            while (index != -1)
+            {
+                var currentNumber = numbers[index];
+                longestSeq.Push(currentNumber);
+                index = prevIndices[index];
+            }
+
+            Console.WriteLine(string.Join(" ", longestSeq));
+        }
+    }
+}
