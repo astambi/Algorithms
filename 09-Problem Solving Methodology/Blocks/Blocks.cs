ef043b54edf07c7c17563dc04e482a51687dc394
Anna <annsta@gmail.com>
2018-05-01 22:35:24 +0300

    Problem Solving Methodology

---
+++ 09-Problem Solving Methodology/Blocks/Blocks.cs
@@ -0,0 +1,99 @@
+﻿namespace Blocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Blocks
+    {
+        private const int LettersToChoose = 4;
+
+        private static readonly HashSet<string> usedCombinations = new HashSet<string>();
+
+        public static void Main()
+        {
+            var numberOfLetters = int.Parse(Console.ReadLine());
+            var results = FindBlocksRecursion(numberOfLetters);
+            Print(results);
+        }
+
+        private static void Print(HashSet<string> results)
+        {
+            Console.WriteLine($"Number of blocks: {results.Count}");
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private static HashSet<string> FindBlocksRecursion(int numberOfLetters)
+        {
+            var letters = GenerateLetters(numberOfLetters);
+            var usedLetters = new bool[numberOfLetters];
+
+            var results = new HashSet<string>();
+            GenerateVariations(new char[LettersToChoose], letters, usedLetters, results);
+            return results;
+        }
+
+        private static void GenerateVariations(char[] combination, char[] letters, bool[] usedLetters, HashSet<string> results, int index = 0)
+        {
+            if (index >= combination.Length)
+            {
+                AddResult(combination, results);
+            }
+            else
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (!usedLetters[i])
+                    {
+                        // Mark used
+                        usedLetters[i] = true;
+
+                        combination[index] = letters[i];
+                        GenerateVariations(combination, letters, usedLetters, results, index + 1);
+
+                        // Unmark used
+                        usedLetters[i] = false;
+                    }
+                }
+            }
+        }
+
+        private static void AddResult(char[] result, HashSet<string> results)
+        {
+            var rotatedStr = RotateBlock(result);
+            if (!usedCombinations.Contains(rotatedStr))
+            {
+                results.Add(rotatedStr);
+                usedCombinations.Add(rotatedStr);
+            }
+        }
+
+        private static string RotateBlock(char[] block)
+        {
+            var startLetter = block.Min(e => e);
+            var rotated = block.ToArray();
+
+            while (rotated[0] != startLetter) // ordered alphabetically
+            {
+                rotated = new[] { rotated[2], rotated[0], rotated[3], rotated[1] };
+            }
+
+            return new string(rotated);
+        }
+
+        private static char[] GenerateLetters(int n)
+        {
+            var letters = new char[n];
+            for (int i = 0; i < n; i++)
+            {
+                letters[i] = (char)(i + 'A');
+            }
+
+            return letters;
+        }
+    }
+}
---
+++ 09-Problem Solving Methodology/BlocksOptimized/BlocksOptimized.cs
@@ -0,0 +1,47 @@
+﻿namespace BlocksOptimized
+{
+    using System;
+
+    public class BlocksOptimized
+    {
+        public static void Main()
+        {
+            var numberOfLetters = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Number of blocks: " +
+                numberOfLetters * (numberOfLetters - 1) * (numberOfLetters - 2) * (numberOfLetters - 3) / 4); // ignoring rotations
+
+            FindBlocksIteration(numberOfLetters);
+        }
+
+        private static void FindBlocksIteration(int numberOfLetters)
+        {
+            var firstLetter = 'A';
+            var lastLetter = firstLetter + numberOfLetters - 1;
+
+            for (char i1 = firstLetter; i1 <= lastLetter; i1++)
+            {
+                for (char i2 = (char)(i1 + 1); i2 <= lastLetter; i2++)
+                {
+                    for (char i3 = (char)(i1 + 1); i3 <= lastLetter; i3++)
+                    {
+                        if (i2 == i3)
+                        {
+                            continue;
+                        }
+
+                        for (char i4 = (char)(i1 + 1); i4 <= lastLetter; i4++)
+                        {
+                            if (i2 == i4 || i3 == i4)
+                            {
+                                continue;
+                            }
+
+                            Console.WriteLine($"{i1}{i2}{i3}{i4}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
