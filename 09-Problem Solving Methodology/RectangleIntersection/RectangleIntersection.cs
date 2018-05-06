90505ceb4095f2947155dc4c5e6ac1a89abcd78d
Anna Stambolieva <annsta@gmail.com>
2018-05-07 02:09:55 +0300

    Problem SolvingMethodology

---
+++ 09-Problem Solving Methodology/RectangleIntersection/RectangleIntersection.cs
@@ -0,0 +1,143 @@
+﻿namespace RectangleIntersection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RectangleIntersection
+    {
+        public static void Main()
+        {
+            var rectangles = ReadRectangles();
+
+            var xCoordinates = rectangles
+                .Select(r => r.MinX)
+                .Union(rectangles.Select(r => r.MaxX))
+                .Distinct()
+                .OrderBy(x => x)
+                .ToList();
+
+            // Calc intersecting rectangles at each X interval
+            var xRect = GetIntersectingRectanglesByX(rectangles, xCoordinates);
+
+            long totalArea = 0;
+            for (int i = 0; i < xRect.Length; i++)
+            {
+                var currentXRect = xRect[i];
+                if (currentXRect.Count < 2) // no X intersecting rectangles
+                {
+                    continue;
+                }
+
+                var yCoordinates = currentXRect
+                    .Select(r => r.MinY)
+                    .Union(currentXRect.Select(r => r.MaxY))
+                    .Distinct()
+                    .OrderBy(y => y)
+                    .ToList();
+
+                // Calc intersecting rectangles at each Y interval
+                var intersectingCount = CalcIntersectingCountByY(currentXRect, yCoordinates);
+
+                for (int j = 0; j < intersectingCount.Length; j++)
+                {
+                    if (intersectingCount[j] < 2) // no Y intersecting rectangles
+                    {
+                        continue;
+                    }
+
+                    var intersection = new Rectangle(
+                        xCoordinates[i], xCoordinates[i + 1],
+                        yCoordinates[j], yCoordinates[j + 1]);
+
+                    totalArea += intersection.Area;
+                }
+            }
+
+            Console.WriteLine(totalArea);
+        }
+
+        private static int[] CalcIntersectingCountByY(List<Rectangle> currentXRect, List<int> yCoordinates)
+        {
+            var intersectingCount = new int[yCoordinates.Count - 1];
+            for (int y = 0; y < yCoordinates.Count; y++)
+            {
+                foreach (var rectangle in currentXRect)
+                {
+                    if (rectangle.MaxY > yCoordinates[y]
+                        && rectangle.MinY < yCoordinates[y + 1]) // intersecting by Y
+                    {
+                        intersectingCount[y]++;
+                    }
+                }
+            }
+
+            return intersectingCount;
+        }
+
+        private static List<Rectangle>[] GetIntersectingRectanglesByX(Rectangle[] rectangles, List<int> xCoordinates)
+        {
+            var xRect = new List<Rectangle>[xCoordinates.Count - 1];
+            for (int i = 0; i < xRect.Length; i++)
+            {
+                xRect[i] = new List<Rectangle>();
+
+                foreach (var rectangle in rectangles)
+                {
+                    if (rectangle.MinX < xCoordinates[i + 1]
+                        && rectangle.MaxX > xCoordinates[i]) // intersecting by X
+                    {
+                        xRect[i].Add(rectangle);
+                    }
+                }
+            }
+
+            return xRect;
+        }
+
+        private static Rectangle[] ReadRectangles()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var rectangles = new Rectangle[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var minX = tokens[0];
+                var maxX = tokens[1];
+                var minY = tokens[2];
+                var maxY = tokens[3];
+
+                rectangles[i] = new Rectangle(minX, maxX, minY, maxY);
+            }
+
+            return rectangles;
+        }
+
+        private class Rectangle
+        {
+            public Rectangle(int minX, int maxX, int minY, int maxY)
+            {
+                this.MinX = minX;
+                this.MaxX = maxX;
+                this.MinY = minY;
+                this.MaxY = maxY;
+            }
+
+            public int MinX { get; }
+
+            public int MaxX { get; }
+
+            public int MinY { get; }
+
+            public int MaxY { get; }
+
+            public int Area
+                => Math.Abs((this.MaxX - this.MinX) * (this.MaxY - this.MinY));
+        }
+    }
+}
---
+++ 09-Problem Solving Methodology/ShortestPathInMatrix/ShortestPathInMatrix.cs
@@ -0,0 +1,174 @@
+﻿namespace ShortestPathInMatrix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    public class ShortestPathInMatrix
+    {
+        private const int Infinity = int.MaxValue;
+
+        private static int[][] matrix;
+        private static int[][] distances;
+        private static Node[][] prevNodes;
+
+        public static void Main()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            ReadMatrix(rows);
+
+            CalcShortestPathsDijkstra();
+
+            ReconstructPath(rows - 1, cols - 1);
+        }
+
+        private static void ReconstructPath(int row, int col)
+        {
+            var destinationNode = new Node(row, col);
+            if (distances[row][col] == Infinity)
+            {
+                return; // no path
+            }
+
+            var path = new Stack<int>();
+            while (destinationNode != null)
+            {
+                path.Push(matrix[destinationNode.Row][destinationNode.Col]);
+                destinationNode = prevNodes[destinationNode.Row][destinationNode.Col];
+            }
+
+            Console.WriteLine($"Length: {path.Sum()}");
+            Console.WriteLine($"Path: {string.Join(" ", path)}");
+        }
+
+        private static void CalcShortestPathsDijkstra()
+        {
+            InitializeDistances(); // infinity
+            InitializePrevNodes(); // null
+
+            var priorityQueue = new OrderedBag<Node>(
+                Comparer<Node>.Create((a, b) =>
+                {
+                    var compare = distances[a.Row][a.Col] - distances[b.Row][b.Col]; // min distance
+                    if (compare == 0) { compare = a.Row - b.Row; } // top to bottom
+                    if (compare == 0) { compare = a.Col - b.Col; } // left to right
+                    return compare;
+                }));
+
+            priorityQueue.Add(new Node(0, 0)); // starting node
+
+            while (priorityQueue.Any())
+            {
+                var minNode = priorityQueue.RemoveFirst();
+
+                var minNodeDistance = distances[minNode.Row][minNode.Col];
+                if (minNodeDistance == Infinity)
+                {
+                    break; // all nodes traversed
+                }
+
+                var neighbours = GetNeighbours(minNode.Row, minNode.Col);
+
+                foreach (var neighbour in neighbours)
+                {
+                    // Enqueque unvisited nodes
+                    var neighbourDistance = distances[neighbour.Row][neighbour.Col];
+                    if (neighbourDistance == Infinity)
+                    {
+                        priorityQueue.Add(neighbour);
+                    }
+
+                    // Improve distance from minNode to neighbour
+                    var newDistance = minNodeDistance + matrix[neighbour.Row][neighbour.Col];
+                    if (newDistance < neighbourDistance)
+                    {
+                        distances[neighbour.Row][neighbour.Col] = newDistance;
+                        prevNodes[neighbour.Row][neighbour.Col] = minNode;
+
+                        // Reorder Queue
+                        priorityQueue = new OrderedBag<Node>(priorityQueue,
+                            Comparer<Node>.Create((a, b) =>
+                            {
+                                var compare = distances[a.Row][a.Col] - distances[b.Row][b.Col];
+                                if (compare == 0) { compare = a.Row - b.Row; }
+                                if (compare == 0) { compare = a.Col - b.Col; }
+                                return compare;
+                            }));
+                    }
+                }
+            }
+        }
+
+        private static void InitializePrevNodes()
+        {
+            prevNodes = new Node[matrix.Length][];
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                prevNodes[row] = new Node[matrix[row].Length]; // null => no prev node
+            }
+        }
+
+        private static void InitializeDistances()
+        {
+            distances = new int[matrix.Length][];
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                distances[row] = Enumerable.Repeat(Infinity, matrix[row].Length).ToArray();
+            }
+
+            distances[0][0] = 0; // starting node
+        }
+
+        private static List<Node> GetNeighbours(int row, int col)
+        {
+            var neighbours = new List<Node>();
+
+            AddNeighbour(row, col - 1, neighbours);
+            AddNeighbour(row, col + 1, neighbours);
+            AddNeighbour(row - 1, col, neighbours);
+            AddNeighbour(row + 1, col, neighbours);
+
+            return neighbours;
+        }
+
+        private static void AddNeighbour(int row, int col, List<Node> neighbours)
+        {
+            if (IsValidCell(row, col))
+            {
+                neighbours.Add(new Node(row, col));
+            }
+        }
+
+        private static bool IsValidCell(int row, int col)
+            => row >= 0 && row < matrix.Length
+            && col >= 0 && col < matrix[row].Length;
+
+        private static void ReadMatrix(int rows)
+        {
+            matrix = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+        }
+
+        private class Node
+        {
+            public Node(int row, int col)
+            {
+                this.Row = row;
+                this.Col = col;
+            }
+
+            public int Row { get; }
+
+            public int Col { get; }
+        }
+    }
+}
