64dea53c2c66cf498c10dc3f093e2ba0b5a39ef4
Anna Stambolieva <annsta@gmail.com>
2018-05-08 08:36:29 +0300

    Proble Solving

---
+++ 10-Problem Solving Part I/Parentheses/Parentheses.cs
@@ -0,0 +1,65 @@
+﻿namespace Parentheses
+{
+    using System;
+    using System.Text;
+
+    public class Parentheses
+    {
+        private const char LeftParenthesis = '(';
+        private const char RightParenthesis = ')';
+
+        private static StringBuilder result = new StringBuilder();
+
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var variation = new char[n * 2];
+
+            VariationsOptimized(0, variation, 0, 0);
+            //VariationsOptimized(0, n, n, variation);
+
+            Console.WriteLine(result.ToString().Trim());
+        }
+
+        private static void VariationsOptimized(int index, char[] variation, int usedLeft, int usedRight)
+        {
+            if (index >= variation.Length)
+            {
+                result.AppendLine(new string(variation));
+                return;
+            }
+
+            if (usedLeft < variation.Length / 2)
+            {
+                variation[index] = LeftParenthesis;
+                VariationsOptimized(index + 1, variation, usedLeft + 1, usedRight);
+            }
+
+            if (usedLeft > usedRight)
+            {
+                variation[index] = RightParenthesis;
+                VariationsOptimized(index + 1, variation, usedLeft, usedRight + 1);
+            }
+        }
+
+        private static void VariationsOptimized(int index, int remainingLeft, int remainingRight, char[] variation)
+        {
+            if (remainingLeft == 0 && remainingRight == 0)
+            {
+                result.AppendLine(new string(variation));
+            }
+
+            if (remainingLeft > 0)
+            {
+                variation[index] = LeftParenthesis;
+                VariationsOptimized(index + 1, remainingLeft - 1, remainingRight, variation);
+            }
+
+            if (remainingLeft < remainingRight)
+            {
+                variation[index] = RightParenthesis;
+                VariationsOptimized(index + 1, remainingLeft, remainingRight - 1, variation);
+            }
+        }
+    }
+}
---
+++ 10-Problem Solving Part I/ParkingZones/ParkingZones.cs
@@ -0,0 +1,147 @@
+﻿namespace ParkingZones
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ParkingZones
+    {
+        public static void Main()
+        {
+            var zones = ReadParkingZones();
+            var freeSpots = ReadParkingSpots(zones);
+            var target = ReadTargetSpot();
+            var traversalTime = int.Parse(Console.ReadLine()); // traversal time of 1 parking spot in secs
+
+            var bestSpot = freeSpots
+                .Where(s => s.CalcPrice(target, traversalTime)
+                        == freeSpots.Select(x => x.CalcPrice(target, traversalTime)).Min()) // min price
+                .OrderBy(s => s.CalcTime(target, traversalTime)) // min time
+                .FirstOrDefault();
+
+            if (bestSpot != null)
+            {
+                Console.WriteLine(bestSpot + $" Price: {bestSpot.CalcPrice(target, traversalTime):f2}");
+            }
+        }
+
+        private static ParkingSpot ReadTargetSpot()
+        {
+            var tokens = Console.ReadLine()
+                .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            return new ParkingSpot(tokens[0], tokens[1]);
+        }
+
+        private static List<ParkingSpot> ReadParkingSpots(Zone[] zones)
+        {
+            var tokens = Console.ReadLine()
+                .Split(new char[] { ';', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var parkingSpots = new List<ParkingSpot>();
+            for (int i = 0; i < tokens.Length; i += 2)
+            {
+                var parkingSpot = new ParkingSpot(tokens[i], tokens[i + 1]);
+
+                // Set Parking Zone
+                foreach (var zone in zones)
+                {
+                    if (zone.Contains(parkingSpot))
+                    {
+                        parkingSpot.ParkingZone = zone;
+                        break;
+                    }
+                }
+
+                parkingSpots.Add(parkingSpot);
+            }
+
+            return parkingSpots;
+        }
+
+        private static Zone[] ReadParkingZones()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var zones = new Zone[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(new char[] { ':', ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var name = tokens[0];
+                var x = int.Parse(tokens[1]);
+                var y = int.Parse(tokens[2]);
+                var width = int.Parse(tokens[3]);
+                var height = int.Parse(tokens[4]);
+                var price = double.Parse(tokens[5]);
+
+                zones[i] = new Zone(name, x, y, width, height, price);
+            }
+
+            return zones;
+        }
+
+        private class Zone
+        {
+            public Zone(string name, int x, int y, int width, int height, double price)
+            {
+                this.Name = name;
+                this.X = x;
+                this.Y = y;
+                this.Width = width;
+                this.Height = height;
+                this.PricePerMin = price;
+            }
+
+            public string Name { get; }
+
+            public int X { get; } // left
+
+            public int Y { get; } // top
+
+            public int Width { get; }
+
+            public int Height { get; }
+
+            public double PricePerMin { get; }
+
+            public bool Contains(ParkingSpot spot)
+                => this.X <= spot.X && spot.X <= this.X + this.Width
+                && this.Y <= spot.Y && spot.Y <= this.Y + this.Height;
+        }
+
+        private class ParkingSpot
+        {
+            public ParkingSpot(int x, int y, Zone zone = null)
+            {
+                this.X = x;
+                this.Y = y;
+                this.ParkingZone = zone;
+            }
+
+            public int X { get; }
+
+            public int Y { get; }
+
+            public Zone ParkingZone { get; set; }
+
+            public double CalcDistance(ParkingSpot other)
+                => Math.Abs(this.X - other.X) + Math.Abs(this.Y - other.Y) - 1;
+
+            public double CalcTime(ParkingSpot other, int traversalTime)
+                => this.CalcDistance(other) * 2 * traversalTime;
+
+            public double CalcPrice(ParkingSpot other, int traversalTime)
+                => Math.Ceiling(this.CalcTime(other, traversalTime) / 60.0) * this.ParkingZone.PricePerMin;
+
+            public override string ToString()
+                => $"Zone Type: {this.ParkingZone.Name}; X: {this.X}; Y: {this.Y};";
+        }
+    }
+}
