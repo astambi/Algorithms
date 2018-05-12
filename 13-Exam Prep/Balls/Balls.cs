aa8c31bf373cb302b5829881843cf9142f6f31ca
Anna Stambolieva <annsta@gmail.com>
2018-05-13 01:40:39 +0300

    Exam Prep

---
+++ 13-Exam Prep/Balls/Balls.cs
@@ -0,0 +1,51 @@
+﻿namespace Balls
+{
+    using System;
+    using System.Text;
+
+    public class Balls
+    {
+        private static int pockets;
+        private static int capacity;
+        private static int[] result;
+        private static StringBuilder builder = new StringBuilder();
+
+        public static void Main()
+        {
+            pockets = int.Parse(Console.ReadLine());
+            var balls = int.Parse(Console.ReadLine());
+            capacity = int.Parse(Console.ReadLine());
+
+            result = new int[pockets];
+
+            Generate(0, balls);
+
+            Console.WriteLine(builder.ToString().Trim());
+        }
+
+        private static void Generate(int index, int ballsLeft)
+        {
+            if (index == pockets)
+            {
+                if (ballsLeft == 0)
+                {
+                    builder.AppendLine(string.Join(", ", result));
+                }
+
+                return;
+            }
+
+            var ballsToPut = ballsLeft - (pockets - (index + 1));
+            if (ballsToPut > capacity)
+            {
+                ballsToPut = capacity;
+            }
+
+            for (int i = ballsToPut; i >= 1; i--)
+            {
+                result[index] = i;
+                Generate(index + 1, ballsLeft - i);
+            }
+        }
+    }
+}
---
+++ 13-Exam Prep/GreatestStrategy/GreatestStrategy.cs
@@ -0,0 +1,120 @@
+﻿namespace GreatestStrategy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GreatestStrategy
+    {
+        private static Dictionary<int, HashSet<int>> graph = new Dictionary<int, HashSet<int>>();
+        private static Dictionary<int, HashSet<int>> modified = new Dictionary<int, HashSet<int>>();
+
+        public static void Main()
+        {
+            var tokens = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var nodesCount = tokens[0];
+            var edgesCount = tokens[1];
+            var root = tokens[2];
+
+            BuildGraph(nodesCount, edgesCount);
+
+            SplitTreeDFS(root, 0, new HashSet<int>());
+
+            var maxValue = GetMaxValue();
+            Console.WriteLine(maxValue);
+        }
+
+        private static int GetMaxValue()
+        {
+            var maxValue = 0;
+            var visited = new HashSet<int>();
+
+            foreach (var node in modified.Keys)
+            {
+                if (!visited.Contains(node))
+                {
+                    var value = GetValueDFS(node, visited);
+                    if (value > maxValue)
+                    {
+                        maxValue = value;
+                    }
+                }
+            }
+
+            return maxValue;
+        }
+
+        private static int GetValueDFS(int node, HashSet<int> visited)
+        {
+            visited.Add(node);
+            var value = node;
+
+            foreach (var child in modified[node])
+            {
+                if (!visited.Contains(child))
+                {
+                    value += GetValueDFS(child, visited);
+                }
+            }
+
+            return value;
+        }
+
+        private static int SplitTreeDFS(int node, int parent, HashSet<int> visited)
+        {
+            visited.Add(node);
+            var totalNodes = 1;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited.Contains(child)
+                    && child != parent)
+                {
+                    var subTreeNodes = SplitTreeDFS(child, node, visited);
+                    totalNodes += subTreeNodes;
+
+                    // Disconnect subtree of even nodes
+                    if (subTreeNodes % 2 == 0)
+                    {
+                        modified[node].Remove(child);
+                        modified[child].Remove(node);
+                    }
+                }
+            }
+
+            return totalNodes;
+        }
+
+        private static void BuildGraph(int nodesCount, int edgesCount)
+        {
+            // Initialize 
+            for (int node = 1; node <= nodesCount; node++)
+            {
+                graph[node] = new HashSet<int>();
+                modified[node] = new HashSet<int>();
+            }
+
+            // Read input
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var edgeTokens = Console.ReadLine()
+                     .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToArray();
+
+                var start = edgeTokens[0];
+                var end = edgeTokens[1];
+
+                graph[start].Add(end);
+                graph[end].Add(start);
+
+                modified[start].Add(end);
+                modified[end].Add(start);
+            }
+        }
+    }
+}
